6f8cd41eb7df37fdc122ef87db9ef93b6afbf209
saaddamou1-lab <saad.damou1@gmail.com>
2026-04-10 00:26:08 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,22 @@
-self.addEventListener('install', e => {
-self.skipWaiting();
+const CACHE_NAME = "trs-app-v1";
+
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll([
+        "./",
+        "./index.html",
+        "./manifest.json",
+        "./icon.png"
+      ]);
+    })
+  );
 });
 
-self.addEventListener('fetch', e => {
-e.respondWith(fetch(e.request));
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(response => {
+      return response || fetch(e.request);
+    })
+  );
 });
